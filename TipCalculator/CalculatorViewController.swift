8f8c1da1d016941436d8c73de01a7a0b216bc83f
spvagelis <spirou.vagelis@gmail.com>
2020-08-05 11:43:38 +0300

    Create IBOutlets and IBActions for Calculator View Controller.

---
+++ TipCalculator/CalculatorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by vagelis spirou on 4/8/20.
+//  Copyright © 2020 vagelis spirou. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPercentButton: UIButton!
+    @IBOutlet weak var tenPercentButton: UIButton!
+    @IBOutlet weak var twentyPercentButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    @IBAction func tipChanged() {
+        
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+}
+
